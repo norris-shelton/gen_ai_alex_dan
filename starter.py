eb1249effa83d0921c9c21b65371ab6608510610
sheltonn <norris.shelton@gmail.com>
2025-11-24 19:08:46 -0400

    first commit

--- starter.py
+++ starter.py
@@ -1,2 +1,26 @@
 import boto3
+import pprint
 
+bedrock = boto3.client(
+    service_name='bedrock', 
+    region_name='us-east-1')
+
+pp = pprint.PrettyPrinter(depth=4, indent=4)
+
+def list_foundation_models():
+    models = bedrock.list_foundation_models()
+    for model in models['modelSummaries']:
+        pp.pprint(model)
+        pp.pprint("--------------------------")
+
+def get_foundation_model(modelIdentifier):
+    model = bedrock.get_foundation_model(
+        modelIdentifier=modelIdentifier
+    )
+    pp.pprint(model)
+
+
+list_foundation_models()
+
+get_foundation_model('anthropic.claude-opus-4-5-20251101-v1:0')
+# pp.print(models)
